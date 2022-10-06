7a2badebf4127a3bbe8789af2961e69bf6581f7d
modani <modani130484@gmail.com>
2022-10-06 10:52:52 +0700

    -update code by modani

--- test_hello.py
+++ test_hello.py
@@ -1,19 +1,8 @@
-from hello import toyou, add, subtract
+from click.testing import CliRunner
+from hello import hello
 
-
-def setup_function(function):
-    print(" Running Setup: %s " % function.__name__)
-    function.x = 10
-
-
-def teardown_function(function):
-    print(" Running Teardown:%s" % function.__name__)
-    del function.x
-
-
-### Run to see failed test
-#def test_hello_add():
-#    assert add(test_hello_add.x) == 12
-
-def test_hello_subtract():
-    assert subtract(test_hello_subtract.x) == 9
+def test_hello():
+    runner = CliRunner()
+    result = runner.invoke(hello, ["--name", "Thor",
+        "--color", "blue"])
+    assert "Thor" in result.output
